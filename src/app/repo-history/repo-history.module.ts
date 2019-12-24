34350a858856a873ad7f778bea80b6ab3b0a1ab9
Oleksii Chuian <oleksii@Oleksiis-MacBook-Pro.local>
2019-12-24 12:02:54 +0200

    [master] CommitsListComponent generated

--- src/app/repo-history/repo-history.module.ts
+++ src/app/repo-history/repo-history.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CommitsListComponent } from './component/commits-list/commits-list.component';
 import { RepoViewComponent } from './component/repo-view/repo-view.component';
 
 import { RepoHistoryService } from './services/repo-history.service';
 
 @NgModule({
   declarations: [
+    CommitsListComponent,
     RepoViewComponent,
   ],
   imports: [
