cf1b309c6b69e820f3b2c22d4272e9ad30b740c2
Oleksii Chuian <aleksey.chuian@gmail.com>
2019-12-24 19:08:57 +0200

    [test-coverage] CommitCardComponent test coverage updated

--- src/app/repo-history/components/commit-card/commit-card.component.spec.ts
+++ src/app/repo-history/components/commit-card/commit-card.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 
 import { CommitCardComponent } from './commit-card.component';
+import { testCommit, testUrl } from '../../../test-data';
 
 describe('CommitCardComponent', () => {
   let component: CommitCardComponent;
@@ -18,10 +19,43 @@ describe('CommitCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CommitCardComponent);
     component = fixture.componentInstance;
+    component.commit = testCommit;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render card title', () => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('mat-card-title').textContent).toContain(testCommit.commit.message);
+  });
+
+  it('should render card subtitle', () => {
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('mat-card-subtitle').textContent).toContain(testCommit.commit.committer.name);
+  });
+
+  describe('link button', () => {
+    it('should render correct link href', () => {
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('a').href).toContain(testUrl);
+    });
+
+    it('should opens in a new tab', () => {
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('a').target).toBe('_blank');
+    });
+
+    it('should have required attribute rel', () => {
+      const compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled.querySelector('a').rel).toBe('noopener noreferrer');
+    });
   });
 });
--- src/app/repo-history/services/repository.service.spec.ts
+++ src/app/repo-history/services/repository.service.spec.ts
@@ -3,34 +3,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 import { RepositoryService } from './repository.service';
 import Commit from '../interfaces';
+import { testCommit } from '../../test-data';
 
-const url = 'test.com';
-const sha = '5d81a6bbc084';
-const expectedCommits: Commit[] = [{
-  sha,
-  commit: {
-    author: {
-      name: 'Test User',
-      email: 'test+user@users.noreply.github.com',
-      date: '2019-12-24T14:13:55Z',
-    },
-    committer: {
-      name: 'GitHub',
-      email: 'noreply@github.com',
-      date: '2019-12-24T14:13:55Z',
-    },
-    message: '[branch] comment to the commit',
-    tree: {
-      sha,
-      url
-    },
-    url,
-    comment_count: 0,
-  },
-  url,
-  html_url: url,
-  comments_url: url,
-}];
+const expectedCommits: Commit[] = [testCommit];
 
 describe('RepoHistoryService', () => {
   let httpClientSpy: { get: jasmine.Spy };
