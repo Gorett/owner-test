66a0b3649d00b5c2c4908a2b3b68a20bdb1da4a1
Oleksii Chuian <aleksey.chuian@gmail.com>
2019-12-24 19:38:19 +0200

    [master] TODO comment added to the CommitViewComponent

--- src/app/repo-history/components/commit-view/commit-view.component.ts
+++ src/app/repo-history/components/commit-view/commit-view.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-commit-view',
   templateUrl: './commit-view.component.html',
   styleUrls: ['./commit-view.component.less']
 })
-export class CommitViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CommitViewComponent {
+  // TODO this component should be implemented when commit detailed view will be requested
 }
