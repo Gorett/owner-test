da8f678971d99a85bf16584abaf54774e00388c5
Oleksii Chuian <oleksii@Oleksiis-MacBook-Pro.local>
2019-12-24 12:03:48 +0200

    [master] CommitViewComponent generated

---
+++ src/app/repo-history/component/commit-view/commit-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commit-view',
+  templateUrl: './commit-view.component.html',
+  styleUrls: ['./commit-view.component.less']
+})
+export class CommitViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/repo-history/repo-history.module.ts
+++ src/app/repo-history/repo-history.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CommitsListComponent } from './component/commits-list/commits-list.component';
+import { CommitViewComponent } from './component/commit-view/commit-view.component';
 import { RepoViewComponent } from './component/repo-view/repo-view.component';
 
 import { RepoHistoryService } from './services/repo-history.service';
@@ -9,6 +10,7 @@ import { RepoHistoryService } from './services/repo-history.service';
 @NgModule({
   declarations: [
     CommitsListComponent,
+    CommitViewComponent,
     RepoViewComponent,
   ],
   imports: [
